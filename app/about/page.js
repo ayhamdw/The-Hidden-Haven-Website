bc837b53e5eeec76dc4c8c7550600a6ed90bd290
ayhamdw <s12029113@stu.najah.edu>
2025-05-22 15:40:16 +0300

    Structure the project

---
+++ app/about/page.js
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+export const metadata = {
+  title: "About",
+};
+
+export default function Page() {
+  return (
+    <div>
+      <h1>Cabins page</h1>
+    </div>
+  );
+}
