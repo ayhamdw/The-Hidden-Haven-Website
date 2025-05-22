b81b5f8c53063b6b7d02787a6c97dd41dffadca9
ayhamdw <s12029113@stu.najah.edu>
2025-05-22 17:09:13 +0300

    Add Tailwind with react taidwind icons

--- app/layout.js
+++ app/layout.js
@@ -1,20 +1,20 @@
 import Logo from "./_components/Logo";
 import Navigation from "./_components/Navigation";
-
+import "@/app/_styles/globals.css";
 export const metadata = {
-  title: "The Wild Oasis",
+  title: "The Hidden Haven",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
+      <body className="bg-primary-950 text-primary-100 min-h-screen">
         <header>
           <Logo />
           <Navigation />
         </header>
         <main>{children}</main>
-        <footer>Copyright by The Wild Oasis</footer>
+        <footer>Copyright by The Hidden Haven</footer>
       </body>
     </html>
   );
