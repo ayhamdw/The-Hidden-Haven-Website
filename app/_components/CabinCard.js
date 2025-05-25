d339a2dba00a037483169faa3b10a2e8c4a55fd1
ayhamdw <s12029113@stu.najah.edu>
2025-05-25 15:42:48 +0300

    Finish Cabin Details Page

--- app/_components/CabinCard.js
+++ app/_components/CabinCard.js
@@ -4,7 +4,6 @@ import { UsersIcon } from "@heroicons/react/24/solid";
 
 function CabinCard({ cabin }) {
   const { id, name, maxCapacity, regularPrice, discount, image } = cabin;
-  console.log(image);
 
   return (
     <div className="flex border-primary-800 border">
