839c64c9935476abfac52c002b4ddb9d3dd959ea
ayhamdw <s12029113@stu.najah.edu>
2025-05-29 15:32:37 +0300

    Enable authentication using google provider

--- app/_components/Reservation.js
+++ app/_components/Reservation.js
@@ -1,12 +1,17 @@
+import { getServerSession } from "next-auth";
 import { getBookedDatesByCabinId, getSettings } from "../_lib/data-service";
 import DateSelector from "./DateSelector";
 import ReservationForm from "./ReservationForm";
+import { authConfig } from "../_lib/auth";
+import LoginMessage from "./LoginMessage";
 
 async function Reservation({ cabin }) {
   const [settings, bookedDates] = await Promise.all([
     getSettings(),
     getBookedDatesByCabinId(cabin.id),
   ]);
+
+  const session = await getServerSession(authConfig);
   return (
     <div className="flex flex-col md:flex-row gap-8 border border-primary-800 rounded-2xl overflow-hidden shadow-lg min-h-[400px] bg-primary-900/80">
       <div className="flex-1 flex flex-col justify-center">
@@ -15,7 +20,11 @@ async function Reservation({ cabin }) {
           bookedDates={bookedDates}
           cabin={cabin}
         />
-        <ReservationForm cabin={cabin} />
+        {session?.user ? (
+          <ReservationForm cabin={cabin} user={session.user} />
+        ) : (
+          <LoginMessage />
+        )}
       </div>
     </div>
   );
--- app/_lib/auth.js
+++ app/_lib/auth.js
@@ -1,7 +1,7 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
-const authConfig = {
+export const authConfig = {
   providers: [
     GoogleProvider({
       clientId: process.env.AUTH_GOOGLE_ID,
