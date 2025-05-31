2a4c0b8a7c6c1411a8a3259242bebbd9a0d9eb0f
ayhamdw <s12029113@stu.najah.edu>
2025-05-31 18:42:14 +0300

    fix delete not mine booking bug

--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -1,7 +1,7 @@
 "use server";
 
 import { getServerSession } from "next-auth";
-import { updateGuest } from "./data-service";
+import { deleteBooking, getBookings, updateGuest } from "./data-service";
 import { authConfig } from "./auth";
 import { revalidatePath } from "next/cache";
 
@@ -20,4 +20,19 @@ export async function updateProfile(formData) {
 
   await updateGuest(userId, updateData);
   revalidatePath("/account/profile");
+}
+
+export async function deleteReservation(bookingId) {
+  const session = await getServerSession(authConfig);
+  if (!session) throw new Error("You Must be logged in");
+  const guestId = session.user.guestId;
+  const guestBookings = await getBookings(guestId);
+
+  const guestBookingsIds = guestBookings.map((booking) => booking.id);
+
+  if (!guestBookingsIds.includes(bookingId))
+    throw new Error("You are not allowed to delete this reservation");
+
+  await deleteBooking(bookingId);
+  revalidatePath("/account/reservations");
 }
